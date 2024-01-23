0f0adf4b9e02b9fffab08ed2418c649b094e72a0
Alvin <thisiseasytoremember1224@gmail.com>
2024-01-23 09:09:11 -0600

    fin

--- movieProject/ViewController.swift
+++ movieProject/ViewController.swift
@@ -24,16 +24,26 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var ghostLabel: UILabel!
-    
+    var x = "Ghost"
+    @IBOutlet weak var textFieldOutlet: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         getMovie()
     }
+    
+    @IBAction func searchButton(_ sender: UIButton) {
+        x = textFieldOutlet.text!
+        getMovie()
+    }
+    
+    
     func getMovie(){
         let session = URLSession.shared
         
-        let movieURL = URL(string: "http://www.omdbapi.com/?i=tt3896198&apikey=78c4cb11")
+       let movieURL = URL(string: "http://www.omdbapi.com/?t=\(x)&apikey=78c4cb11&")
+        
+
         
         let dataTask = session.dataTask(with: movieURL!) { [self] (data:Data?, response:URLResponse?, error:Error?) in
             if let e = error{
@@ -47,30 +57,35 @@ class ViewController: UIViewController {
                         print(jsonObj)
                         
                         //get Movie object with JSONDecoder
-                    if let movieObj = try?
-                       JSONDecoder().decode(Movie.self, from: data!){
-                        print(movieObj.Actors)
-                        DispatchQueue.main.async{
-                            self.ghostLabel.text = "\(movieObj.Year)"
-                        }
-                        for r in movieObj.Ratings{
-                            print("\(r.Source)  \(r.Value)")
-                        }
-                    }
-                        
-                    else{
-                        print("error decoding to movie object")
-                    }
-                        
-                        
-                   //     if let y = jsonObj.value(forKey: "Ghost") as? NSDictionary{
-                
-                   //        if let ghost = y.value(forKey: "Year") as? Int{
-                    //            DispatchQueue.main.async{
-                     //               self.ghostLabel.text = "\(y)"
-                   //             }
-                   //         }
+                  //  if let movieObj = try?
+                //       JSONDecoder().decode(Movie.self, from: data!){
+                 //       print(movieObj.Actors)
+                 //       DispatchQueue.main.async{
+                   //         self.ghostLabel.text = "\(movieObj.Year)"
                    //     }
+                   //     for r in movieObj.Ratings{
+                   //         print("\(r.Source)  \(r.Value)")
+                  //      }
+                 //   }
+                        
+                    
+                        
+
+                               
+                        if let y = jsonObj.value(forKey: "Year") as? String{
+                                    
+                            DispatchQueue.main.async{
+                                self.ghostLabel.text = "Movie Year: \(y)"
+                                        }
+                                   }
+                                
+                        else{
+                            DispatchQueue.main.async{
+                                self.ghostLabel.text = "please insert a valid title"
+                            }
+                        }
+                        
+                        
                         
                         
                         
